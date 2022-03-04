d74b3ef149d7c8ef50e25f993332d5afa47d68a1
Minnie <kikilala.hsuan@gmail.com>
2022-03-04 15:39:36 +0800

    scoringNG

--- src/com/le/ScorinrNG.java
+++ src/com/le/ScorinrNG.java
@@ -6,7 +6,9 @@ public class ScorinrNG {
         int[] english={70,90,60};
         int[]math={60,55,80};
         for(int i=0;i<3;i++){
-            System.out.println(names[i] + "\t" + english[i] + "\t" + math[i] + "\t" + (english[i]+math[i])/2 );
+            if (i!=1) {
+                System.out.println(names[i] + "\t" + english[i] + "\t" + math[i] + "\t" + (english[i] + math[i]) / 2);
+            }
         }
     }
 }
