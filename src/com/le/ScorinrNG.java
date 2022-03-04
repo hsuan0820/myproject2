027ad4d98eb3da8792230b36b68603bdfb589e86
Minnie <kikilala.hsuan@gmail.com>
2022-03-04 15:24:54 +0800

    Initial commit

---
+++ src/com/le/ScorinrNG.java
@@ -0,0 +1,12 @@
+package com.le;
+
+public class ScorinrNG {
+    public static void main(String[] args) {
+        String[]names={"Jack","Benny","Karen"};
+        int[] english={70,90,60,50,30};
+        int[]math={60,55,80,75,85};
+        for(int i=0;i<8;i++){
+            System.out.println(names[i]);
+        }
+    }
+}
