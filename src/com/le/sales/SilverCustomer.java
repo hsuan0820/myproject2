50059ee92eb3941b372572ecae42a13623d02395
hsuan0820 <kikilala.hsuan@gmail.com>
2022-04-02 02:41:04 +0800

    SilverCustomer commit

---
+++ src/com/le/sales/SilverCustomer.java
@@ -0,0 +1,14 @@
+package com.le.sales;
+
+    public class SilverCustomer extends Customer {
+
+        public SilverCustomer(String id, int amount) {
+            super(id, amount);
+        }
+        @Override
+        public void print() {
+            System.out.println(id + "\t" + amount + "\t" +
+                    (amount-backMoney()) +
+                    "(" + backMoney() + ")");
+        }
+    }
