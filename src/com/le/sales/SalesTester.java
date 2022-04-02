2febb81d28602d598b833b4ed204d8e6a961e450
hsuan0820 <kikilala.hsuan@gmail.com>
2022-04-02 11:20:03 +0800

    SalesTester second commit

--- src/com/le/sales/SalesTester.java
+++ src/com/le/sales/SalesTester.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 public class SalesTester {
         public static void main(String[] args) {
-            List<Customer> customers = new ArrayList<>();
+            List <Customer> customers = new ArrayList<>();
             customers.add(new Customer("0001", 1200));
             customers.add(new Customer("0002", 800));
             customers.add(new SilverCustomer("0003", 2000));
             customers.add(new GoldenCustomer("0004", 5000));
-            customers.add(new DiscountCustomer(0006,900));
+            customers.add(new DiscountCustomer("0006",900));
             for (int i = 0; i < 5; i++) {
                 customers.get(i).print();
             }
